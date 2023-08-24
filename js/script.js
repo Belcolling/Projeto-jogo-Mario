40ddebc23ccd05da9ab5ea7ab8d35ccaf41dbf47
Belcolling <Belcolling999@gamil.com>
2023-08-23 22:39:10 -0300

    jogo mario fase terminal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+const mario = document.querySelector('.mario');
+const mario = document.querySelector('.pipe');
+ 
+const jump = () => { 
+    mario.classList.add('jump'); 
 
-const jump = () 
+    setTimeout(() => { 
+        mario.classList.remove('jump');
+    }, 500);
+
+} 
+
+const loop = setInterval(() => {  
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft; 
+    const pipePosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+
+     console.log(marioPosition);
+
+    if(pipePosition ≤ 120 && pipePosition > 0 && marioPosition < 80) { 
+
+        pipe.style.animation = 'none';
+        pipe.style.left = '${pipePosition}px';
+
+        mario.style.animation = 'none';
+        mario.style.bottom = '${marioPosition}px'; 
+
+        mario.src ='./images/game-over.png'; 
+        mario.style.width = '75px' 
+        mario.style.marginLeft = '50px' 
+
+        clearInterval(loop);
+    }
+
+}, 10);
+
 document.addEventListener('keydown', jump);
